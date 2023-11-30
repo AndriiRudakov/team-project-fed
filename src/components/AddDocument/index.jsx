4e1b29f99b638cdfe97debabaac452f168662733
Andrii Rudakov <andruha.nastyle@gmail.com>
2023-11-30 14:53:20 +0200

    added add document functionality

--- src/components/AddDocument/index.jsx
+++ src/components/AddDocument/index.jsx
@@ -1,7 +1,25 @@
 import React from 'react'
-
+import classes from "./AddDocument.module.css";
 const AddDocument = () => {
-    return <div>Add here form for document upload</div>
+    return (
+        <form className={classes.form}>
+            <div className={classes.title}>Додати документ</div>
+          <input
+            className={classes.input}
+            type="string"
+            placeholder="Назва документу"
+          />
+          <input className={classes.inputD} type="String" placeholder="Детальний опис" />
+          <input 
+            className={classes.fileInput}
+            type="file"
+            accept=".pdf, .doc, .docx"  
+          />
+          <button className={classes.submitBtn} type="submit">
+            Submit
+          </button>
+        </form>
+      );
 }
 
 export default AddDocument;
