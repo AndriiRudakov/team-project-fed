02cd7dcc3c8016e0bf15ed87fcf084d8ddd46819
Andrii Rudakov <andruha.nastyle@gmail.com>
2023-12-13 15:37:51 +0200

    Misha changes

--- src/components/SelectOption/index.jsx
+++ src/components/SelectOption/index.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
+import classes from "./SelectOption.module.css";
 
 const SelectOption = () => {
-    return <div>Add here options</div>
-}
+    return (
+        <div className={classes.selectOption}>
+            <div>
+                <button className={classes.button1}>Додати нові документи</button>
+                <button className={classes.button2}>Переглянути свої документи</button>
+            </div>
+        </div>
+    );
+};
 
 export default SelectOption;
